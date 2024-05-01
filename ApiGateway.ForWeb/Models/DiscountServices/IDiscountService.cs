447791df5573807bb75c6c1477cdb846d47d3450
SayyehBan <sdvp1991david@gmail.com>
2024-05-01 19:38:25 +0330

    Add project files.

---
+++ ApiGateway.ForWeb/Models/DiscountServices/IDiscountService.cs
@@ -0,0 +1,104 @@
+﻿using ApiGateway.ForWeb.Models.Dtos;
+using ApiGateway.ForWeb.Models.Links;
+using DiscountService.Proto;
+using Grpc.Net.Client;
+
+namespace ApiGateway.ForWeb.Models.DiscountServices;
+
+public interface IDiscountService
+{
+    ResultDto<DiscountDto> GetDiscountByCode(string Code);
+    ResultDto<DiscountDto> GetDiscountById(Guid Id);
+    ResultDto UseDiscount(Guid DiscountId);
+}
+public class RDiscountService : IDiscountService
+{
+    private readonly IConfiguration configuration;
+    private readonly GrpcChannel channel;
+
+    public RDiscountService()
+    {
+        channel = GrpcChannel.ForAddress(LinkServices.DiscountService);
+    }
+
+
+    public ResultDto<DiscountDto> GetDiscountByCode(string Code)
+    {
+        var grpc_discountService = new DiscountServiceProto.DiscountServiceProtoClient(channel);
+        var result = grpc_discountService.GetDiscountByCode(new RequestGetDiscountByCode
+        {
+            Code = Code
+        });
+
+        if (result.IsSuccess)
+        {
+            return new ResultDto<DiscountDto>
+            {
+                Data = new DiscountDto
+                {
+                    Amount = result.Data.Amount,
+                    Code = result.Data.Code,
+                    Id = Guid.Parse(result.Data.Id),
+                    Used = result.Data.Used
+                },
+                IsSuccess = result.IsSuccess,
+                Message = result.Message,
+            };
+        }
+        return new ResultDto<DiscountDto>
+        {
+            IsSuccess = false,
+            Message = result.Message,
+        };
+    }
+
+    public ResultDto<DiscountDto> GetDiscountById(Guid Id)
+    {
+        var grpc_discountService = new DiscountServiceProto.DiscountServiceProtoClient(channel);
+        var result = grpc_discountService.GetDiscountById(new RequestGetDiscountById
+        {
+            Id = Id.ToString(),
+        });
+
+        if (result.IsSuccess)
+        {
+            return new ResultDto<DiscountDto>
+            {
+                Data = new DiscountDto
+                {
+                    Amount = result.Data.Amount,
+                    Code = result.Data.Code,
+                    Id = Guid.Parse(result.Data.Id),
+                    Used = result.Data.Used
+                },
+                IsSuccess = result.IsSuccess,
+                Message = result.Message,
+            };
+        }
+        return new ResultDto<DiscountDto>
+        {
+            IsSuccess = false,
+            Message = result.Message,
+        };
+    }
+
+    public ResultDto UseDiscount(Guid DiscountId)
+    {
+        var grpc_discountService = new DiscountServiceProto.DiscountServiceProtoClient(channel);
+        var result = grpc_discountService.UseDiscount(new RequestUseDiscount
+        {
+            Id = DiscountId.ToString(),
+        });
+        return new ResultDto
+        {
+            IsSuccess = result.IsSuccess
+        };
+    }
+}
+public class DiscountDto
+{
+    public int Amount { get; set; }
+    public string Code { get; set; }
+    public Guid Id { get; set; }
+    public bool Used { get; set; }
+}
